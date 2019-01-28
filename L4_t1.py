7bc6512493220abfad56600561f184c377e77917
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-01-28 13:25:16 -0800

    Completed reading file line by line

--- L4_t1.py
+++ L4_t1.py
@@ -1,1 +1,9 @@
 # LAb4 Task1:Write a program that reads a file, breaks each line into words, strips whitespace and punctuation from the words, and converts them to lowercase 
+
+
+
+fin=open("mybook.txt")
+for line in fin:
+    word=line.strip()
+    print(word)
+
